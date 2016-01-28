891e4abca9bfff0f7be72c50a00aa883b2597fde
Ryan Meredith <ryan.d.meredith@googlemail.com>
2016-01-28 12:43:25 +0100

    libgdx setup including overlap2D and html

---
+++ html/src/uk/co/adeveloperabroad/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package uk.co.adeveloperabroad.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import uk.co.adeveloperabroad.Loader;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Loader();
+        }
+}
